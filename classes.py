76be518c9bc5a8f1dad98232533b394207940ecb
DrSphinx <brunocabeludo321@gmail.com>
2020-09-03 03:09:33 -0300

    Renomear arquivo fonte das classes.

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
-# BRUNO DE OLIVEIRA 31/08/2020
-# MICOODISSEY Future Bio
-
-import time
+# BRUNO DE OLIVEIRA 31/08/2020 <- data de inicio.
+# MICOODISSEY Future B
 
 import pygame as pg
 import json
 from random import randint
+
+
+def ocupa_interior(self, particula):
+    self.interior[particula.id] = particula
 
 
 class Celula(object):
@@ -24,10 +26,10 @@ class Celula(object):
         self.parede_celular = ParedeCelular()
 
     def transporte_passivo(self):
-        #tempo = time.perf_counter()
-        #incial = tempo
+        # tempo = time.perf_counter()
+        # incial = tempo
         ##CONTROLE DE TRANSPORTE INTRA/EXTRA-CELULAR. NÃO MEXER!
-        #DE: CARIOTECA
+        # DE: CARIOTECA
         for id, particula in self.carioteca.interior.copy().items():
             if particula.transportando:
                 self.migrar(self.citoplasma, self.carioteca, particula)
@@ -43,8 +45,7 @@ class Celula(object):
                 if particula.destino == "Cloroplasto":
                     self.migrar(self.cloroplastos, self.citoplasma, particula)
 
-
-        #DE: CITOPLASMA
+        # DE: CITOPLASMA
         for id, particula in self.citoplasma.interior.copy().items():
             if particula.transportando:
                 if particula.destino == "Carioteca":
@@ -61,7 +62,7 @@ class Celula(object):
                 else:
                     self.migrar(self.cloroplastos, self.citoplasma, particula)
 
-        #DE: MEMBRANA
+        # DE: MEMBRANA
         for id, particula in self.membrana.interior.copy().items():
             if particula.transportando:
                 if particula.destino == "Parede":
@@ -77,7 +78,7 @@ class Celula(object):
                     elif particula.destino == "Cloroplasto":
                         self.migrar(self.cloroplastos, self.citoplasma, particula)
 
-        #print(f'Tempo laço: {time.perf_counter() - incial}')
+        # print(f'Tempo laço: {time.perf_counter() - incial}')
 
     def gera_energia(self):
         self.citoplasma.energia += self.cloroplastos.fotossintese(self.fonte_luz)
@@ -100,12 +101,6 @@ class Celula(object):
             self.gera_energia()
 
 
-
-
-def ocupa_interior(self, particula):
-    self.interior[particula.id] = particula
-    
-    
 class Carioteca(object):
     def __init__(self):
         self.interior = {}
@@ -117,15 +112,16 @@ class Citoplasma(object):
         self.interior = {}
         self.energia = 0
         self.pilha = []
-    
+
     def pedir_energia(self, receptor, importancia, quantidade, retorno=False):
-        self.pilha.append({"receptor": receptor, "importancia": importancia, "quantidade": quantidade, "retorno": retorno})
+        self.pilha.append(
+            {"receptor": receptor, "importancia": importancia, "quantidade": quantidade, "retorno": retorno})
         return self.conceder_energia()
 
     def conceder_energia(self):
         for pedido in self.pilha:
             pedido["importancia"] -= 1
-            maior = {"importancia": 0} #maior importancia
+            maior = {"importancia": 0}  # maior importancia
         for i, pedido in enumerate(self.pilha):
             if pedido["importancia"] > maior["importancia"]:
                 maior = pedido
@@ -135,7 +131,7 @@ class Citoplasma(object):
             self.energia -= pedido["quantidade"]
             if pedido["retorno"]:
                 pedido["receptor"].energia += pedido["quantidade"]
-            del(self.pilha[i])
+            del (self.pilha[i])
             return True
         else:
             return False
@@ -153,6 +149,7 @@ class ParedeCelular(object):
         self.receptores = {}
         self.emissor = {}
         self.energia = 0
+
 
 class Cloroplasto(object):
     def __init__(self):
@@ -201,10 +198,11 @@ class Cloroplasto(object):
                 if particula.nome == substancia:
                     self.interior.pop(particula.id)
                     ct += 1
-            else: break
+            else:
+                break
 
     def draw(self, display, cell_rect):
-        self.rect.center = (cell_rect.x + cell_rect.w /2, cell_rect.y + cell_rect.h / 2 + 3)
+        self.rect.center = (cell_rect.x + cell_rect.w / 2, cell_rect.y + cell_rect.h / 2 + 3)
         pg.draw.ellipse(display, self.cor, self.rect, 5)
         pg.time.wait(50)
         self.cor = (0, 255, 0)
@@ -216,7 +214,7 @@ class Particula(object):
         self.xna_tile = xna_tile
         self.transportando = transportando
         self.destino = destino
-        self.id = f"{randint(0,9)}{randint(0,9)}{randint(0,9)}"
+        self.id = f"{randint(0, 9)}{randint(0, 9)}{randint(0, 9)}"
 
 
 class FonteLuz(object):
